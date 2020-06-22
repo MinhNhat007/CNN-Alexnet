f986898569a033a814acf976e149fda4d35e8202
Minh Nhat Trinh <n_cqt@yahoo.com>
2020-06-23 00:14:40 +0200

    hello

--- main.py
+++ main.py
@@ -46,3 +46,10 @@ cnn.summary()
 cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 history = cnn.fit(x = training_set, validation_data = test_set, epochs = 50)
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.0, 1])
+plt.legend(loc='lower right')
